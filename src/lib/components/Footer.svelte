a9019b55cf0e9761316f7789c708725288582b1d
Kaminoatowo <j.cocomello@gmail.com>
2024-02-25 16:28:43 +0100

    added instagram logo

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,9 +1,17 @@
+<script>
+    import { InstagramIcon } from 'svelte-uicons/brands';
+</script>
+
 <footer class="wrapper">
     <hr class="border-1 border-white w-full mx-auto">
-    <div class="bg-secondary text-quaternary my-2 mx-5 text-center">
-        <p class="m-0 ml-auto mr-auto text-center">© 2024 OGD design 🦡
+    <div class="bg-secondary text-quaternary my-2 mx-5 text-center flex justify-center">
+        <p class="m-0 ml-auto mr-auto text-center"> Oltre gli Dei © Copyright 2023  DEV
             <br>
-            All rights reserved.
+            Sito web creato da OGD design 🦡
         </p>
+        
+        <a href="https://www.instagram.com/oltre_gli_dei/" target="_blank" class="m-4">
+            <InstagramIcon size="24" />
+        </a>
     </div>
 </footer>
