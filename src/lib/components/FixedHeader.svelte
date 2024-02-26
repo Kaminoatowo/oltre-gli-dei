e3182da615b146250f485667d0934995dd8e7448
Kaminoatowo <j.cocomello@gmail.com>
2024-02-26 18:35:42 +0100

    insert title sticky header

--- src/lib/components/FixedHeader.svelte
+++ src/lib/components/FixedHeader.svelte
@@ -1,13 +1,14 @@
 <script>
     import Navbar from "./Navbar.svelte";
     import { ChessIcon } from "svelte-uicons/rounded/regular";
+    import OgdIcon from "./OGDIcon.svelte";
     
     export let visibility;
 </script>
 
 <header class="h-{visibility} wrapper text-secondary font-bold truncate sticky top-0 flex justify-center">
-    <a href="/" class="h-5 p-4 sm:p-10">
-        <ChessIcon size="32"/>
+    <a href="/" class="h-5 mt-2 sm:p-10">
+        <OgdIcon size="160"/>
     </a>
     <Navbar />
 </header>
