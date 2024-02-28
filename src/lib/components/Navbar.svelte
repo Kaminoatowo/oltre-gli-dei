7b1e05351407c93633243be5b685cd74e760d9c9
Kaminoatowo <j.cocomello@gmail.com>
2024-02-28 17:01:18 +0100

    add sticky nav

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,39 +1,13 @@
 <script>
-    let customClass = "";
-    export { customClass as class };
+    import NavbarLinks from "./NavbarLinks.svelte";
+    import OgdIcon from "./OGDIcon.svelte";
 </script>
 
-<nav class="h-5 p-4 sm:p-10 {customClass}">
-    <ul class="list-none flex flex-wrap justify-center gap-x-4">
-        <div class="text-center">
-            <li>
-                <a href="/blog" class="px-3">Blog</a>
-            </li>
-            <li>
-                <a href="/tarots" class="px-3">Tarocchi</a>
-            </li>
-            <li>
-                <a href="/about" class="px-3">About</a>
-            </li>
-        </div>
-        
-    </ul>
-</nav>
+<div class="wrapper text-secondary font-bold truncate sticky top-0 flex justify-between">
+    <a href="/" class="hidden mt-auto sm:p-10">
+        <OgdIcon size="160"/>
+    </a>
+    <NavbarLinks class="mx-auto mt-2" />
+</div>
 
-<style lang="postcss">
-
-    li {
-        float: left;
-    }
-
-    a {
-		text-decoration: none;
-		color: inherit;
-        display: block;
-        text-align: center;
-	}
-
-    a:hover {
-        background-color: bg-tertiary;
-    }
-</style>
+<hr class="border-1 border-white w-full mx-auto">
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,10 @@
 <script>
   import Header from "$lib/components/Header.svelte";
-  import FixedHeader from "../lib/components/FixedHeader.svelte";
+  import Navbar from "../lib/components/Navbar.svelte";
   import Footer from "$lib/components/Footer.svelte";
   import '$lib/styles/style.css';
   import "../app.css";
   import { fade } from 'svelte/transition';
-  import StickyHeader from "../lib/components/StickyHeader.svelte";
 
   export let data;
   let visibility = "hidden";
@@ -13,8 +12,7 @@
 
 <body class="bg-primary bg-cover text-white text-xs sm:text-base">
   <Header />
-  <!--FixedHeader visibility="h-14 sm:h-24"/-->
-  <StickyHeader stickToTop={true} />
+  <Navbar />
 
   {#key data.currentRoute}
   <main class="wrapper my-0" in:fade={{ duration: 150, delay: 150}} out:fade={{ duration:150}}>
