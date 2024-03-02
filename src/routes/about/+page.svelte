51923f150e6f390bdf07d0cba9407749fa669cdd
Kaminoatowo <j.cocomello@gmail.com>
2024-03-02 19:19:35 +0100

    add about description

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,6 +3,17 @@
     <title>About - Jacopo Cocomello</title>
 </svelte:head>
 
-<h1>Hi, I'm Jacopo!</h1>
+<h1 class="my-2">Chi sono</h1>
 
-<p>This is my about page.</p>
+<div class="m-2 p-2">
+    <h2>Ti spiego qualcosa su di me</h2> 
+    
+</div>
+
+<hr class="border-1 border-white w-full mx-auto my-2">
+
+<div class="bg-tertiary text-quinternary m-4 p-2 rounded-md">
+    <p>Sono un'entità anarco-spirituale che ha imparato a riconoscersi
+        nella propria manifestazione corporea.
+    </p>
+</div>
