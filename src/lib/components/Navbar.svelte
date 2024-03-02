97b27ac23bf7e2c73fcfb23369bac027aa6d7b48
Kaminoatowo <j.cocomello@gmail.com>
2024-03-02 18:15:34 +0100

    possible solution with #if

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,6 +1,22 @@
 <script>
     import NavbarLinks from "./NavbarLinks.svelte";
     import OgdIcon from "./OGDIcon.svelte";
+    /*import { onMount } from "svelte";
+
+    let topPosition = 0;
+
+    function getParentTopPosition() {
+        const parentDiv = document.querySelector(".link");
+        const { top } = parentDiv.getBoundingClientRect();
+        topPosition = top;
+    }
+
+    onMount(getParentTopPosition);
+    {#if topPosition == 0}
+        <a href="/" class="link mt-auto sm:p-10">
+            <OgdIcon size="160"/>
+        </a>
+    {/if}*/
 </script>
 
 <div class="wrapper text-secondary font-bold truncate sticky top-0 flex justify-between">
